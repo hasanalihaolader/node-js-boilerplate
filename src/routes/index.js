009f833c4b5e494da36668b2383c34d83a95e5a4
bs23.hasan <hasan.haolader@brainstation-23.com>
2024-06-06 02:58:33 +0600

    feat: update application title

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'STS ( Support ticket management system )' });
+  res.render('index', { title: 'Node js boilerplate ( with database configuration and auth )' });
 });
 
 
