4c05d01cfb229c84d3ac02e43776d6bfc249de51
bs23.hasan <hasan.haolader@brainstation-23.com>
2024-06-08 19:25:39 +0600

    feat: make api for get users and user profile

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,8 +3,54 @@ const helper = require("../helper/helper");
 const userRepository = require("../repository/userRepository");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const logger = require('../utils/logger');
 
 const UserController = {
+    getUsers: async (req, res, next) => {
+        let statusCode = 200;
+        let message = 'User data fetch successfully';
+        let context = {};
+        try {
+            let user = await userRepository.findAll();
+            if (user.length > 0) {
+                context = user;
+            }
+            else {
+                statusCode = 404;
+                message = 'User data not found';
+            }
+        } catch (error) {
+            console.log(error);
+            statusCode = 500;
+            context = {
+                name: error.name,
+                errors: [{
+                    message: error.errors[0].message,
+                    type: error.errors[0].type
+                }]
+            };
+        }
+        return res.status(statusCode).json(
+            helper.formatApiResponse(
+                statusCode,
+                message,
+                context
+            )
+        );
+    },
+    userProfile: async (req, res, next) => {
+        const token = req.header('Authorization');
+        if (!token) {
+            return res.status(401).json(helper.formatApiResponse(401, 'Access Denied. No refresh token provided.'));
+        }
+        try {
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            const user = await userRepository.findById(decoded.userId);
+            res.status(200).json(helper.formatApiResponse(200, 'User profile fetch successfully', { 'user': user }));
+        } catch (error) {
+            return res.status(400).json(helper.formatApiResponse(400, 'User profile fetch failed.'));
+        }
+    },
     create: async (req, res, next) => {
         let statusCode = 500;
         let message = 'Failed to create user';
--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,4 +1,5 @@
-const userModel = require("../model/userModel");
+const models = require('../models');
+const userModel = models.user;
 
 const userRepository = {
     store: async (user) => {
@@ -10,6 +11,14 @@ const userRepository = {
     },
     findByUserName: async (userName) => {
         return await userModel.findOne({ where: { user_name: userName, status: 1} });
-    }
+    },
+    findById: async (id) => {
+        return await userModel.findByPk(id);
+    },
+    findAll: async () => {
+        return await userModel.findAll({
+            order: [['id', 'DESC']]
+        });
+    },
 }
 module.exports = userRepository;
